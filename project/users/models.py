371552a9cf9ab7529cf4a8eb5f28c4cc1dbfa441
Eyad (ED) <eak1u19@soton.ac.uk>
2019-12-05 20:50:24 +0000

    Extending User model

--- project/users/models.py
+++ project/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), blank=True, null=True, unique=True)
+    REQUIRED_FIELDS = ['email']
