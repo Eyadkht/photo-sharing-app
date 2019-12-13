50d3b374722b15c2d7646264897323ae482a5df7
aa1g19 <55995541+aa1g19@users.noreply.github.com>
2019-12-13 16:32:38 +0000

    Add files via upload
    I have completed the admin dashboard so that it has the edit event page and a logout button. Also incorporates the routing mechanism from before.

--- frontend/login/adminDashboard/adminDashboardController.js
+++ frontend/login/adminDashboard/adminDashboardController.js
@@ -1,6 +1,7 @@
-adminDashboardModule.controller("adminDashboardController", function ($scope, $http) {
-
+adminDashboardModule.controller("adminDashboardController", function($scope, $http){
+	
 	$scope.title = "Events";
+	
 	console.log("In dashboard controller");
 
 	// GET events created
@@ -19,43 +20,70 @@ adminDashboardModule.controller("adminDashboardController", function ($scope, $h
 		// called asynchronously if an error occurs
 		// or server returns response with an error status.
 	});
-
-
-
+	
+	
 	$scope.events =
-		[
-			{
-				name: "New Years Party",
-				date: "31-12-2019",
-				active: true,
-				pin: "1234",
-				description: "This is a new years eve party, etc ...",
-				location: "London, England"
-			},
-			{
-				name: "Christmas Party",
-				date: "25-12-2019",
-				active: true,
-				pin: "4321",
-				description: "This is a Christmas party, etc ...",
-				location: "London, England"
-			},
+	[
+		{
+			name: "New Years Party",
+			date: "2019-12-31",
+			active: true,
+			pin: "1234",
+			description: "This is a new years eve party, etc ...",
+			location: "London, England"},
+		 {
+			name: "Christmas Party",
+			date: "2019-12-25",
+			active: true,
+			pin: "4321",
+			description: "This is a Christmas party, etc ...",
+			location: "London, England"},
+		 
+		 {
+			name: "Summer Event",
+			date: "2020-06-20",
+			active: false,
+			pin: "6789",
+			description: "This is a summer event, etc ...",
+			location: "Paris, France"},
+	];
+	
+	// This function is used to display the information of a specified event in the edit pane
+	$scope.displayEvent = function(eventID)
+	{
+		document.getElementById('eventName').value = $scope.events[eventID].name;
+		document.getElementById('eventDate').value = $scope.events[eventID].date;
+		document.getElementById('eventIsActive').checked = $scope.events[eventID].active;
+		document.getElementById('eventPin').value = $scope.events[eventID].pin;
+		document.getElementById('eventDescription').value = $scope.events[eventID].description;
+		document.getElementById('eventLocation').value = $scope.events[eventID].location;
+	}
+	
+	// This function will make a request to the back end to create a new event
+	$scope.createEvent = function()
+	{
+		
+	}
+	
+	// This function will make a request to the back end to modify a specific event
+	$scope.saveEvent = function(eventID)
+	{
+		
+	}
+	
+	$scope.deleteEvent = function(eventID)
+	{
+		alert("Event deleted.");
+	}
+	
+	// Simple Function for showing the logout button
+	$scope.showLogout = function()
+	{
+		document.getElementById('logoutButton').style.display = "block";
+	}
 
-			{
-				name: "Summer Event",
-				date: "20-06-2020",
-				active: false,
-				pin: "6789",
-				description: "This is a summer event, etc ...",
-				location: "Paris, France"
-			},
-		];
-
-	// Creating constants representing the page state (either a login page or a register page)
-	$scope.LOGIN = 0;
-	$scope.REGISTER = 1;
-
-	$scope.LoginOrRegister = $scope.LOGIN;
-
-
+	$scope.logout = function()
+	{
+		document.getElementById('logoutButton').style.display = "none";
+	}
 });
