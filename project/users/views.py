b28bbcdf553aa424a0d1584d93b62b48628c359d
Eyad (ED) <eak1u19@soton.ac.uk>
2019-12-14 02:12:28 +0000

    Create a view to check GAE env value

--- project/users/views.py
+++ project/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import os
+from django.http import HttpResponse
+    
 # Create your views here.
+def testing_endpoint(request):
+    if os.getenv('GAE_APPLICATION', None):
+        x = os.getenv('GAE_APPLICATION', None)
+        return HttpResponse(x)
+    else:
+        return HttpResponse("no GAE")
