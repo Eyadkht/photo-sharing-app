f94ec224b8e81333a0cc955ebba8516694c0755a
Eyad (ED) <eak1u19@soton.ac.uk>
2019-12-26 00:43:34 +0000

    Create Interaction with media to increase likes API

--- project/api/events/views.py
+++ project/api/events/views.py
@@ -1,9 +1,10 @@
+from django.db import transaction
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-
 from project.api.events.serializers import ImageSerializer
+from project.events.models import Image
 
 
 class ImageUploadView(APIView):
@@ -18,3 +19,25 @@ class ImageUploadView(APIView):
           return Response(image_serializer.data, status=status.HTTP_201_CREATED)
       else:
           return Response(image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MediaInteraction(APIView):
+    @transaction.atomic
+    def put(self, request):
+       
+        media_id = request.data['image_id']
+
+        # Checking if in Image object with the requested id exists
+        try:
+            media = Image.objects.get(pk=int(media_id))
+        except Image.DoesNotExist:
+            content = {'details': "Media does not exist"}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        
+        #  Increasing the total number of likes by 1
+        media.likes = media.likes + 1
+        likes = media.likes
+        media.save()
+
+        content = {"new_likes":likes}
+        
+        return Response(content, status=status.HTTP_200_OK)
--- project/api/urls.py
+++ project/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from project.api.users.views import SnippetDetail,SnippetList
-from project.api.events.views import ImageUploadView
+from project.api.events.views import ImageUploadView,MediaInteraction
 
 urlpatterns = [
     path('users/', SnippetList.as_view()),
     path('users/<int:pk>', SnippetDetail.as_view()),
-    path('upload_image/', ImageUploadView.as_view())
+    path('upload_image/', ImageUploadView.as_view()),
+    path('interact_media/',MediaInteraction.as_view()),
 ]
