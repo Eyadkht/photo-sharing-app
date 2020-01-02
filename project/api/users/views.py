abd59eb6277b889a3901c84bb04b7269a66f9925
Eyad (ED) <eak1u19@soton.ac.uk>
2020-01-02 22:58:18 +0000

    Add authentication permession to UserDetail API

--- project/api/users/views.py
+++ project/api/users/views.py
@@ -1,13 +1,14 @@
 from project.users.models import User
 from project.api.users.serializers import UserSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 
-class SnippetList(generics.ListCreateAPIView):
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.filter(is_staff=False)
     serializer_class = UserSerializer
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = User.objects.filter(is_staff=False)
     serializer_class = UserSerializer
